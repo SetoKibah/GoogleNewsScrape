fc50c05d10d5f42be917ce58d91f4e2c755b980c
Bradley <setokibah@gmail.com>
2021-02-25 12:44:55 +0000

    Testing pygooglenews

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from requests_html import HTMLSession
+from pygooglenews import GoogleNews
 
+# Interesting tool that might be helpful to use https://github.com/kotartemiy/pygooglenews
+
+# Testing template given on the github page
+gn = GoogleNews()
+
+search = gn.search('lockdown')
+
+for item in search['entries']:
+  print(item['title'])
+
+# Template is clean and works well, will further improve
+
+
+"""
 url = 'https://news.google.com/rss/search?q=lockdown'
 
 # Create our session
@@ -11,3 +26,4 @@ r = s.get(url)
 # Testing title acquisition.
 for title in r.html.find('title'):
   print(title.text)
+  """
